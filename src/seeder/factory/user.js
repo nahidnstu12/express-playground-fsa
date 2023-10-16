f4b4ddd046610f1c24397c3ab5972b00c45a91a3
nahidnstu12 <nahid.dev19@gmail.com>
2023-10-16 17:48:28 +0600

    seeding implementation refactoring as requirement

--- src/seeder/factory/user.js
+++ src/seeder/factory/user.js
@@ -4,11 +4,26 @@ const { USER_ROLES, USER_STATUS } = require("../../utils/constants");
 
 exports.UsersFactory = setSeederFactory(User, async (faker) => {
   const user = {};
+  delete USER_ROLES.APP_ADMIN;
+  let modifiedUserRoles = USER_ROLES;
   user.name = faker.internet.userName();
   user.phone = faker.phone.number();
   user.email = faker.internet.email();
   user.password = "$10$uTq3dlNf7kjuBD8upY.JveFblB8QwZKHfUR13i/GTv9e1tbEuH3Qm"; //121212aA
-  user.role = faker.helpers.enumValue(USER_ROLES);
+  user.role = faker.helpers.enumValue(modifiedUserRoles);
   user.status = faker.helpers.enumValue(USER_STATUS);
   return user;
 });
+
+exports.appAdminUser = {
+  name: "App Admin",
+  email: "app@mail.com",
+  phone: "01621871111",
+  password: "$10$uTq3dlNf7kjuBD8upY.JveFblB8QwZKHfUR13i/GTv9e1tbEuH3Qm",
+  role: USER_ROLES.APP_ADMIN,
+  status: USER_STATUS.APPROVED,
+};
+
+exports.getAdminUser = (users, role) => {
+  return users.filter((user) => user.role == role);
+};
--- src/seeder/index.js
+++ src/seeder/index.js
@@ -3,9 +3,12 @@ const User = require("../model/user");
 const Menu = require("../model/menu");
 const Cart = require("../model/cart");
 const Order = require("../model/order");
+const { USER_ROLES } = require("../utils/constants");
+const { appAdminUser, getAdminUser } = require("./factory/user");
 
 class MainSeeder {
   async run(dataSource, factoryManager) {
+    const userRepository = dataSource.getRepository(User);
     const menuRepository = dataSource.getRepository(Menu);
     const cartRepository = dataSource.getRepository(Cart);
     const orderRepository = dataSource.getRepository(Order);
@@ -15,14 +18,17 @@ class MainSeeder {
     const cartFactory = factoryManager.get(Cart);
     const orderFactory = factoryManager.get(Order);
 
-    const users = await userFactory.saveMany(5);
-
+    await userRepository.save(appAdminUser);
+    const users = await userFactory.saveMany(10);
+    const adminUsers = getAdminUser(users, USER_ROLES.ADMIN);
+    const customerUsers = getAdminUser(users, USER_ROLES.CUSTOMER);
+    // console.log("users=======", users, adminUsers, customerUsers);
     const menus = await Promise.all(
       Array(10)
         .fill("")
         .map(async () => {
           return await menuFactory.make({
-            userId: faker.helpers.arrayElement(users),
+            userId: faker.helpers.arrayElement(adminUsers),
           });
         }),
     );
@@ -33,7 +39,7 @@ class MainSeeder {
         .fill("")
         .map(async () => {
           return await cartFactory.make({
-            userId: faker.helpers.arrayElement(users),
+            userId: faker.helpers.arrayElement(customerUsers),
             menuId: faker.helpers.arrayElement(menuList),
           });
         }),
@@ -45,7 +51,7 @@ class MainSeeder {
         .fill("")
         .map(async () => {
           return await orderFactory.make({
-            userId: faker.helpers.arrayElement(users),
+            userId: faker.helpers.arrayElement(customerUsers),
             menuId: faker.helpers.arrayElement(menuList),
           });
         }),
